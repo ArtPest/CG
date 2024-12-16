917d91d2c08ce7257b2e50848c2dd9c4b19a309c
ArtPest <98692916+ArtPest@users.noreply.github.com>
2024-12-16 19:17:53 +0300

    Update main.py

--- lw_6/main.py
+++ lw_6/main.py
@@ -5,21 +5,17 @@ import math
 from map import world_map
 from drawing import Drawing
 
-# Инициализация Pygame и звука
 pygame.init()
 pygame.mixer.init()
 
-# Настройки экрана и времени
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-# Игрок и рисование
 player = Player()
 drawing = Drawing(sc)
 
-# Загрузка фоновой музыки и звуков шагов
 pygame.mixer.music.load('audio/darklabirynth.wav')
-pygame.mixer.music.play(-1)  # -1 для зацикливания
+pygame.mixer.music.play(-1)
 
 footstep_sound = pygame.mixer.Sound('audio/footstep_echoed.wav')
 
@@ -28,17 +24,14 @@ while True:
         if event.type == pygame.QUIT:
             exit()
 
-    # Движение игрока и шаги
     player_speed_before = player.speed
     player.movement()
 
-    # Воспроизведение шагов, если игрок движется
-    if player.speed > 0 and player_speed_before == 0:  # Игрок начал двигаться
-        footstep_sound.play(-1)  # Зацикливаем шаги
-    elif player.speed == 0 and player_speed_before > 0:  # Игрок остановился
+    if player.speed > 0 and player_speed_before == 0:
+        footstep_sound.play(-1)
+    elif player.speed == 0 and player_speed_before > 0:
         footstep_sound.stop()
 
-    # Очистка экрана и рендеринг
     sc.fill((0, 0, 0))
     drawing.background()
     drawing.world(player.pos, player.angle)
